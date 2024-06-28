a335729ff7daaeabaf28cad196774655ac268d6d
Maicol <maicolmike@hotmail.com>
2024-06-28 16:33:30 -0500

    pruebas

--- users/views.py
+++ users/views.py
@@ -6,6 +6,26 @@ from django.contrib.auth.decorators import login_required # vista basada en func
 
 # Create your views here.
 def login_view(request):
-    return render(request, 'users/login.html',{
+    if request.method == 'POST':
+        form = LoginUser(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Bienvenido {}'.format(user.username))
+                return redirect('index')  # Redirige al usuario a la página de inicio
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos')
+    else:
+        form = LoginUser()  # Crea un formulario vacío si la solicitud no es POST
+    
+    # Si el usuario ya está autenticado, redirige a la página de inicio
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    return render(request, 'users/login.html', {
         'title': "Login",
+        'form': form
     })
