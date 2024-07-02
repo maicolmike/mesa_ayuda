768196ca00af3545e0805ec7d37fe142ff404860
Maicol <maicolmike@hotmail.com>
2024-07-02 16:21:38 -0500

    settings

--- mesa_ayuda/settings.py
+++ mesa_ayuda/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'mesa_ayuda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mesa_ayuda',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
