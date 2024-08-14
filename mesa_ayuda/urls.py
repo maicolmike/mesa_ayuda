1e8b49b6df0040b24dfc3059c16abcca91a38f47
Maicol <maicolmike@hotmail.com>
2024-08-14 17:00:46 -0500

    sesion se cierra 5 minutos despues

--- mesa_ayuda/urls.py
+++ mesa_ayuda/urls.py
@@ -5,6 +5,8 @@ from django.urls import include
 from users.views import login_view,logout_view,register,usersList,UsersListView,CambiarClave,UserUpdateView,UserUdpateClave,UserDelete,crear_usuario,editar_usuario
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from django.shortcuts import render
 
 
 urlpatterns = [
@@ -29,3 +31,8 @@ urlpatterns = [
 # para que reconozca los archivos media adjuntos
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#configurar error 404
+def custom_404(request, exception):
+    return render(request, 'errors/404.html', {}, status=404)
+
+handler404 = custom_404
