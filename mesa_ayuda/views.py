3973c6e8acb6c875330391acb564e11375bd735d
Maicol <maicolmike@hotmail.com>
2026-04-06 15:55:59 -0500

    archivo plano

--- mesa_ayuda/views.py
+++ mesa_ayuda/views.py
@@ -2,11 +2,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required # vista basada en funciones que no permita acceder a paginas donde no se ha logeado
+from django.db.models import Count
+from requerimientos.models import Requerimiento
 
 @login_required(login_url='login')
 def index(request):
-    #return HttpResponse('Hola mundo')
-    return render(request, 'index.html',{ 
+    """
+    Dashboard principal del sistema
+    """
+
+    # 🔹 Total de requerimientos
+    total_requerimientos = Requerimiento.objects.count()
+
+    # 🔹 Agrupar por estado (ACTIVO, CERRADO, etc.)
+    por_estado = Requerimiento.objects.values('estado').annotate(
+        total=Count('id')
+    ).order_by('estado')
+
+    # 🔹 Últimos 5 requerimientos (para mostrar en tabla)
+    ultimos = Requerimiento.objects.select_related('usuario').order_by('-id')[:5]
+
+    context = {
+        'total_requerimientos': total_requerimientos,
+        'por_estado': por_estado,
+        'ultimos': ultimos,
         'title': "Index",
-    })
+    }
+
+    return render(request, 'index.html', context)
 
